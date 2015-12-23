a0088831f242f2fa413b0d5c7e05d123a8c8ea35
Ky Waegel <kwaegel@cs.unc.edu>
2015-12-23 02:22:22 -0800

    Added day 15, part 2.

--- day_15/src/main.rs
+++ day_15/src/main.rs
@@ -8,6 +8,10 @@ use std::cmp;
 
 #[macro_use] extern crate itertools;
 
+extern crate rand;
+use rand::Rng;
+use rand::distributions::{IndependentSample, Range};
+
 extern crate regex;
 use regex::Regex;
 
@@ -69,27 +73,62 @@ fn get_ingredients_from_file(filename: &str) -> Vec<Ingredient> {
 
 fn get_score(ingredients: &Vec<Ingredient>, quantities: &Vec<i32>) -> i32 {
 	let mut sub_scores = vec![0i32; 4];
+	let mut calories = 0;
 	for (ing, amount) in ingredients.iter().zip(quantities.iter()) {
 		sub_scores[0] += ing.capacity * amount;
 		sub_scores[1] += ing.durability * amount;
 		sub_scores[2] += ing.flavor * amount;
 		sub_scores[3] += ing.texture * amount;
+		calories += ing.calories * amount;
 	}
-	println!("Subscores are {:?}", sub_scores);
 
-	sub_scores.iter()
-			  .map(|&x| cmp::max(0, x))
-			  .fold(1, |prod, x| prod*x )
+	let raw_score = sub_scores.iter()
+			        .map(|&x| cmp::max(0, x))
+			        .fold(1, |prod, x| prod*x );
+		
+	let final_score = if calories > 500 { 0 } else { raw_score };
+	//let final_score = raw_score;
+	
+	println!("Subscores are {:?} = {} with {} calories = {}", 
+		sub_scores, raw_score, calories, final_score);
+	
+	final_score
+}
+
+// --------------------------------------------------------
+
+fn get_random_quantities<R: Rng>(vec_size: usize, rng: &mut R) -> Vec<i32> {
+	let mut nums = vec![0i32; vec_size];
+	let mut sum = 0;
+	for i in 0..vec_size-1 {
+		let max = 100 - sum;
+		let between = Range::new(0i32, max+1);
+		nums[i] = between.ind_sample(rng);
+		sum += nums[i];
+	}
+	nums[vec_size-1] = 100-sum;
+	
+	let vec_sum = nums.iter().fold(0, |sum, x| sum + x);
+	assert_eq!(100, vec_sum);
+	
+	nums
 }
 
 // --------------------------------------------------------
 
 fn main() {
+	let mut rng = rand::thread_rng();
+
     let ingredients = get_ingredients_from_file("day15.txt");
 	
-	// Start with equal amounts
-	let starting_amt = 100i32 / ingredients.len() as i32;
-	let mut quantities = vec![starting_amt; ingredients.len()];
+	// Pick random quantities until score > 0
+	let mut quantities = Vec::new();	
+	let mut best_score = 0;	
+	while best_score == 0 {
+		// Generate a new initial solution until score > 0
+		quantities = get_random_quantities(ingredients.len(), &mut rng);
+		best_score = get_score(&ingredients, &quantities);
+	}
 	
 	// Solve using hill climbing
 	let mut keep_checking = true;
@@ -123,5 +162,5 @@ fn main() {
 	}
 	
 	let score = get_score(&ingredients, &quantities);
-	println!("Score for {:?} is {}", &quantities, score);
+	println!("Part 2: score for {:?} is {}", &quantities, score);
 }
