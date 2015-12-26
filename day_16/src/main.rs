185a63e4db6a38090c7f13bebda95f5f1a60c55e
Ky Waegel <ky@matterport.com>
2015-12-25 23:41:22 -0500

    Added day 16, part 2.

--- day_16/src/main.rs
+++ day_16/src/main.rs
@@ -10,23 +10,17 @@ use std::collections::HashMap;
 extern crate regex;
 use regex::Regex;
 
+enum Ineq {
+	Equals(i32),
+	GreaterThan(i32),
+	LessThan(i32),
+}
+
 // --------------------------------------------------------
 
-fn main() {
+fn find_sue (constraints: &HashMap<&str, Ineq>, filename: &str) -> i32 {
 
-	let mut my_sue_stats = HashMap::new();
-	my_sue_stats.insert("children", 3);
-	my_sue_stats.insert("cats", 7);
-	my_sue_stats.insert("samoyeds", 2);
-	my_sue_stats.insert("pomeranians", 3);
-	my_sue_stats.insert("akitas", 0);
-	my_sue_stats.insert("vizslas", 0);
-	my_sue_stats.insert("goldfish", 5);
-	my_sue_stats.insert("trees", 3);
-	my_sue_stats.insert("cars", 2);
-	my_sue_stats.insert("perfumes", 1);
-
-    let path = Path::new("day16.txt");
+	let path = Path::new(filename);
     let display = path.display();
 
     // Open the path in read-only mode, returns `io::Result<File>`
@@ -42,18 +36,29 @@ fn main() {
 	let re = Regex::new(r"(?P<key>[:alpha:]+): (?P<value>\d+)").unwrap();
 	
 	let mut sue_num = 0;
+	let mut sue_no_conflict = -1i32;
 	for line in lines {
 		let text = line.unwrap();
 		sue_num += 1;
 		
 		let mut has_conflict = false;
 		for cap in re.captures_iter(&text) {
-			let key =cap.name("key").unwrap_or("");
-			let value =cap.name("value").unwrap_or("").parse::<i32>().unwrap();
+			let key = cap.name("key").unwrap_or("");
+			let value = cap.name("value").unwrap_or("").parse::<i32>().unwrap();
 			
-			match my_sue_stats.get(&key) {
-				Some(&found_value) => {
-					if found_value != value {
+			match constraints.get(&key) {
+				Some(&Ineq::Equals(present_value)) => {
+					if value != present_value {
+						has_conflict = true;
+					}
+				},
+				Some(&Ineq::GreaterThan(present_value)) => {
+					if value <= present_value {
+						has_conflict = true;
+					}
+				},
+				Some(&Ineq::LessThan(present_value)) => {
+					if value >= present_value {
 						has_conflict = true;
 					}
 				},
@@ -63,6 +68,44 @@ fn main() {
 		
 		if !has_conflict {
 			println!("Sue {} has no conflicts", sue_num);
+			sue_no_conflict = sue_num;
 		}
 	}
+	sue_no_conflict
+}
+
+
+// --------------------------------------------------------
+
+fn main() {
+
+	println!("Running part 1...");
+	let mut sue_stats_exact = HashMap::new();
+	sue_stats_exact.insert("children", Ineq::Equals(3) );
+	sue_stats_exact.insert("cats", Ineq::Equals(7) );
+	sue_stats_exact.insert("samoyeds", Ineq::Equals(2) );
+	sue_stats_exact.insert("pomeranians", Ineq::Equals(3) );
+	sue_stats_exact.insert("akitas", Ineq::Equals(0) );
+	sue_stats_exact.insert("vizslas", Ineq::Equals(0) );
+	sue_stats_exact.insert("goldfish", Ineq::Equals(5) );
+	sue_stats_exact.insert("trees", Ineq::Equals(3) );
+	sue_stats_exact.insert("cars", Ineq::Equals(2) );
+	sue_stats_exact.insert("perfumes", Ineq::Equals(1) );
+	
+	find_sue(&sue_stats_exact, "day16.txt");
+	
+	println!("Running part 2...");
+	let mut sue_stats_ineq = HashMap::new();
+	sue_stats_ineq.insert("children", Ineq::Equals(3) );
+	sue_stats_ineq.insert("cats", Ineq::GreaterThan(7) );
+	sue_stats_ineq.insert("samoyeds", Ineq::Equals(2) );
+	sue_stats_ineq.insert("pomeranians", Ineq::LessThan(3) );
+	sue_stats_ineq.insert("akitas", Ineq::Equals(0) );
+	sue_stats_ineq.insert("vizslas", Ineq::Equals(0) );
+	sue_stats_ineq.insert("goldfish", Ineq::LessThan(5) );
+	sue_stats_ineq.insert("trees", Ineq::GreaterThan(3) );
+	sue_stats_ineq.insert("cars", Ineq::Equals(2) );
+	sue_stats_ineq.insert("perfumes", Ineq::Equals(1) );
+	
+	find_sue(&sue_stats_ineq, "day16.txt");
 }
